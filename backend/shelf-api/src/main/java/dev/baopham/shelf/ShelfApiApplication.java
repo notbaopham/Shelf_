cda13ea727c68134026b80ce5194990f4f0e4ae0
notbaopham <113058628+notbaopham@users.noreply.github.com>
2026-05-28 01:15:16 -0700

    feat(back): add backend with Spring Init

---
+++ backend/shelf-api/src/main/java/dev/baopham/shelf/ShelfApiApplication.java
@@ -0,0 +1,12 @@
+package dev.baopham.shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfApiApplication.class, args);
+	}
+}
